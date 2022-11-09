eca5ae883c9a205d6a56a075d0f459af595541b5
fiflokonon <fiflokonon@gmail.com>
2022-11-09 18:58:40 +0100

    invit

--- lib/screens/CreatInvitation.dart
+++ lib/screens/CreatInvitation.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:guests/widgets/InputFormWidget.dart';
-import 'package:number_inc_dec/number_inc_dec.dart';
+
 
 class CreateInvitation extends StatelessWidget {
   const CreateInvitation({super.key});
